bd17c685cfcdb846a1c38b93be4fd23fbcbdacf9
seifeakalu <50824246+seifeakalu@users.noreply.github.com>
2021-08-27 05:07:49 -0700

    Add files via upload

---
+++ components/Posts.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledArticle = styled.div`
+  h1 {
+    font-weight: 700;
+    font-size: 36px;
+    color: #000000;
+    margin: 0;
+  }
+  h4 {
+    font-weight: 700;
+    font-size: 20px;
+    color: #000000;
+    margin: 0.5rem 0 4rem 0;
+  }
+  p {
+    font-weight: 300;
+    font-size: 18px;
+    color: #4a4a4a;
+  }
+`;
+
+const Posts = ({ title = "", posts = [] }) => {
+  return (
+    <StyledArticle>
+      <h1>{title}</h1>
+      {posts.map(paragraph => (
+        <p key={paragraph.text}>{paragraph.text}</p>
+      ))}
+    </StyledArticle>
+  );
+};
+
+export default Posts;
